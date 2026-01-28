291230e7215be3c20ff55f3236ada0f98dc68ece
Manmeet <manmeetkaurnahal1313@gmail.com>
2026-01-28 17:53:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-
 <script>
   const hamburger = document.getElementById("hamburger");
   const navLinks = document.getElementById("navLinks");
+  const links = navLinks.querySelectorAll("a");
 
   hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
     hamburger.classList.toggle("open");
   });
+
+  links.forEach(link => {
+    link.addEventListener("click", () => {
+      navLinks.classList.remove("active");
+      hamburger.classList.remove("open");
+    });
+  });
 </script>
-</body>
