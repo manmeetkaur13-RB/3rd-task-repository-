4cee07b4eb7300d4807c2271c1d199e6baaf8c48
Manmeet <manmeetkaurnahal1313@gmail.com>
2026-01-28 22:25:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const navLinks = document.querySelector(".nav-links");
   const links = document.querySelectorAll(".nav-links a");
 
-  // Safety check (prevents JS errors)
   if (!hamburger || !navLinks) return;
 
-  // Toggle menu on hamburger click
   hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("active-menu");
   });
 
-  // Close menu when a link is clicked
   links.forEach(link => {
     link.addEventListener("click", () => {
       navLinks.classList.remove("active-menu");
